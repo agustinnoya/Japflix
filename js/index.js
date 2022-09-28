ea94889bfacd909507caf38dd47c3680f4ae6021
agustin noya <108200137+Agustinn57@users.noreply.github.com>
2022-09-28 12:53:22 -0300

    fetch
    eso

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+info=""
+
+function traer() {
+    fetch("https://japceibal.github.io/japflix_api/movies-data.json")
+      .then(response => response.json())
+      .then(data => { console.log(data); return data })
+      .then(data => { info = data; return data })
+      .then(info => { console.log("fetch", info); return info })
+      .catch(error => console.log(error))
+  }
+
+traer()
